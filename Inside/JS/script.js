fea7971a8d39aca09971ed47d31b7fb62a9b905c
Sergio Bianchi <sergio.bianchi.2007@calvino.edu.it>
2025-04-02 12:51:57 +0200

    Viva napoli

--- Inside/JS/script.js
+++ Inside/JS/script.js
@@ -6,11 +6,18 @@ $("#choose-overlay > img").click(function() {
     $("#choose-overlay").fadeOut(1000);
 })
 
-
 const scrollCheck = document.querySelector("#scroll-check");
 const keyboardCheck = document.querySelector("#keyboard-check");
-
 
 if(outerHeight != innerHeight || outerWidth != innerWidth || window.devicePixelRatio != 1) {
     console.log("SET FULLSCREEN 100%")
 }
+
+$("#choosed-overlay > img").click(function() {
+    $("#choosed-overlay").addClass("hidden")
+})
+
+$("#packages-container > img").click(function(self) {
+    $(this).addClass("opacity-0")
+    $("#choosed-overlay").removeClass("hidden")
+})
